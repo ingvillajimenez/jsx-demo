ac48a894dd3b42a293ddef0c71891f5549ef6873
Juan Villa <ing.villa.jimenez@gmail.com>
2023-10-12 23:49:10 -0600

    Embedding JavaScript in JSX

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+function getMood() {
+  const moods = ["Angry", "Hungry", "Silly", "Quiet", "Paranoid"];
+  return moods[Math.floor(Math.random() * moods.length)];
+}
+
 class JSXDemo extends React.Component {
   render() {
     return (
-      // <div>
-      //   <h1>My Image!</h1>
-      //   <img src="https://images.unsplash.com/photo-1696986291642-54225330b8d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2671&q=80" />
-      // </div>
-      React.createElement("h3", null, "I AM AN H3")
+      <div>
+        <h1>My Current Mood is: {getMood()}</h1>
+      </div>
     );
   }
 }
